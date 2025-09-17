f9cfa6bd22d9a0fe09a070ed66158dfb9bc71577
NelsonOrtiz94 <ortnn990@gmail.com>
2025-09-17 09:38:35 -0500

    Primer commit

---
+++ src/test/java/com/sistema/pagos/SistemaPagosBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.pagos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaPagosBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
