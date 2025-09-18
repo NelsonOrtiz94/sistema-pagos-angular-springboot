1a272ed4352640332f320e72b30cedc7d5892810
NelsonOrtiz94 <ortnn990@gmail.com>
2025-09-18 16:07:35 -0500

    Se crea paquete web para estructurar PagoController

---
+++ src/main/java/com/sistema/pagos/web/PagoController.java
@@ -0,0 +1,93 @@
+package com.sistema.pagos.web;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.sistema.pagos.entities.Estudiante;
+import com.sistema.pagos.entities.Pago;
+import com.sistema.pagos.enums.PagoStatus;
+import com.sistema.pagos.enums.TypePago;
+import com.sistema.pagos.repository.EstudianteRepository;
+import com.sistema.pagos.repository.PagoRepository;
+import com.sistema.pagos.services.PagoService;
+
+@RestController
+@CrossOrigin("*")
+public class PagoController {
+    
+    @Autowired
+    private EstudianteRepository estudianteRepository;    
+
+    @Autowired
+    private PagoRepository pagoRepository;
+
+    @Autowired
+    private PagoService pagoService;
+
+    @GetMapping("/estudiantes")
+    public List<Estudiante> listarEstudiantes(){
+        return estudianteRepository.findAll();
+    }
+
+    @GetMapping("/estudiantes/{codigo}")
+    public Estudiante listarEstudiantePorCodigo(@PathVariable String codigo){
+        return estudianteRepository.findByCodigo(codigo);
+    }
+
+    @GetMapping("/estudiantesPorPrograma")
+    public List<Estudiante> listarEstudiantesPorPrograma(@RequestParam String programaId){
+        return estudianteRepository.findByProgramaId(programaId);
+    }
+
+    @GetMapping("/pagos")
+    public List<Pago> listarPagos(){
+        return pagoRepository.findAll();
+    }
+
+    @GetMapping("/pagos/{id}")
+    public Pago listarPagoPorId(@PathVariable Long id){
+        return pagoRepository.findById(id).get();
+    }
+
+    @GetMapping("/estudiantes/{codigo}/pagos")
+    public List<Pago> listarPagosPorCodigoEstudiante(@PathVariable String codigo){
+        return pagoRepository.findByEstudianteCodigo(codigo);
+    }
+
+    @GetMapping("/pagosPorStatus")
+    public List<Pago> listarPagosPorStatus(@RequestParam PagoStatus status){
+        return pagoRepository.findByStatus(status);
+    }
+
+    @GetMapping("/pagos/porTipo")
+    public List<Pago> listarPagosPorType(@RequestParam TypePago type){
+        return pagoRepository.findByType(type);
+    }
+
+    @PutMapping("/pagos/{pagoId}/actualizarPago")
+    public Pago actualizarStatusDePago(@RequestParam PagoStatus status,@PathVariable Long pagoId){
+        return pagoService.actualizarPagoPorStatus(status, pagoId);
+    }
+
+    @PostMapping(path = "/pagos",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public Pago guardarPago(@RequestParam("file") MultipartFile file, double cantidad, TypePago type, LocalDate date, String codigoEstudiante) throws IOException{
+        return pagoService.savePago(file, cantidad, type, date, codigoEstudiante);
+    }
+
+    @GetMapping(value = "/pagoFile/{pagoId}",produces = MediaType.APPLICATION_PDF_VALUE)
+    public byte[] listarArchivoPorId(@PathVariable Long pagoId) throws IOException{
+        return pagoService.getArchivoPorId(pagoId);
+    }
+}
